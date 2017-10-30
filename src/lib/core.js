61753646502c206ecdbb4ef6d0009605ab6a75eb
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2017-10-29 21:42:23 -0500

    Added Power Countdown

--- src/lib/core.js
+++ src/lib/core.js
@@ -2,40 +2,37 @@ const $ = require('jquery');
 //Model
 function init() {
     return {
+        Power: 100,
         lookingAt: 'classroom Camera',
-        monsterLocation: 'Lounge',
+        monsterLocation: 'hallway',
         interactables: {
-            fan: 'on', // 'off' // 'powering down',
-            studyDoor: 'closed', // 'open' // 'shaking',
-            loungeDoor: 'closed', // 'open' // 'shaking',
-            safetyDoor: 'closed' // 'open' // 'shaking'
+            snackRoomDoor: 'closed', // 'off' // 'powering down',
+            safetyDoorTwo: 'closed', // 'open' // 'shaking',
+            safetyDoorOne: 'closed' // 'open' // 'shaking'
         }
     };
 }
 //Viewable Cameras
 function Cameras() {
-    return [
-        'classroom Camera',
-        'office Camera',
-        'hallway Camera',
-        'bathroom Camera'
-    ];
+    return ['classroom', 'office', 'hallway', 'bathroom'];
 }
-
-Pictures = {
-    Room: {
-        hallway: '../../assets/fnabc/Hallway.jpeg',
-        bathroom: '../../assets/fnabc/Bathroom.jpeg',
-        office: '../../assets/fnabc/Office.jpeg',
-        classroom: '' // havent taken
-    },
-    MonsterInRoom: {
-        hallway: '../../assets/fnabc/Hallway_with_monster.jpeg',
-        bathroom: '../../assets/fnabc/Bathroom_with_Monster.jpeg',
-        office: '../../assets/fnabc/D6846834-B60A-4E1C-99C6-1721759BF537.jpeg',
-        classroom: '' //hasnt been taken
-    }
-};
+function SceneChange() {
+    return {
+        Room: {
+            hallway: '../../assets/fnabc/Hallway.jpeg',
+            bathroom: '../../assets/fnabc/Bathroom.jpeg',
+            office: '../../assets/fnabc/Office.jpeg',
+            classroom: '' // havent taken
+        },
+        MonsterInRoom: {
+            hallway: '../../assets/fnabc/Hallway_with_monster.jpeg',
+            bathroom: '../../assets/fnabc/Bathroom_with_Monster.jpeg',
+            office:
+                '../../assets/fnabc/D6846834-B60A-4E1C-99C6-1721759BF537.jpeg',
+            classroom: '' //hasnt been taken
+        }
+    };
+}
 
 function lookingAt(State, viewLocation) {
     State.lookingAt = viewLocation;
@@ -52,3 +49,4 @@ exports.lookingAt = lookingAt;
 exports.monsterMove = monsterMove;
 exports.interact = interact;
 exports.Cameras = Cameras;
+exports.SceneChange = SceneChange;
---
+++ src/pages/game/game.js
@@ -0,0 +1,133 @@
+const $ = require('jquery');
+const core = require('../../lib/core');
+
+function view(Model) {
+    if (Model.lookingAt === 'classroom') {
+        if (Model.monsterLocation === 'classroom') {
+            $('#screen').html(
+                screenView(core.SceneChange()['MonsterInRoom']['classroom'])
+            );
+        } else {
+            $('#screen').html(
+                screenView(core.SceneChange()['Room']['classroom'])
+            );
+        }
+    } else if (Model.lookingAt === 'office') {
+        if (Model.monsterLocation === 'office') {
+            $('#screen').html(
+                screenView(core.SceneChange()['MonsterInRoom']['office'])
+            );
+        } else {
+            $('#screen').html(screenView(core.SceneChange()['Room']['office']));
+        }
+    } else if (Model.lookingAt === 'hallway') {
+        if (Model.monsterLocation === 'hallway') {
+            $('#screen').html(
+                screenView(core.SceneChange()['MonsterInRoom']['hallway'])
+            );
+        } else {
+            $('#screen').html(
+                screenView(core.SceneChange()['Room']['hallway'])
+            );
+        }
+    } else if (Model.lookingAt === 'bathroom') {
+        if (Model.monsterLocation === 'bathroom') {
+            $('#screen').html(
+                screenView(core.SceneChange()['MonsterInRoom']['bathroom'])
+            );
+        } else {
+            $('#screen').html(
+                screenView(core.SceneChange()['Room']['bathroom'])
+            );
+        }
+    }
+    gameStart(Model);
+}
+function monsterMove(Model) {
+    if (Model.monsterLocation === 'classroom') {
+        var locations = ChooseMonsterLocation(['classroom', 'hallway']);
+        core.monsterMove(Model, locations);
+    } else if (Model.monsterLocation === 'hallway') {
+        var locations = ChooseMonsterLocation([
+            'classroom',
+            'bathroom',
+            'office'
+        ]);
+        core.monsterMove(Model, locations);
+    } else if (Model.monsterLocation === 'bathroom') {
+        var locations = ChooseMonsterLocation(['hallway']);
+        core.monsterMove(Model, locations);
+    } else if (Model.monsterLocation === 'office') {
+        var locations = ChooseMonsterLocation(['hallway']);
+        core.monsterMove(Model, locations);
+    } else if (Model.monsterLocation === 'safetyDoor') {
+        core.interact(Model, 'safetyDoor', 'shaking');
+        // if after 5 seconds or so the safetyDoor isn't closed then jumpscare
+        // else go to either the office or hallway area
+    }
+}
+function screenView(picture) {
+    return (
+        "<img class='center-block' src='" +
+        picture +
+        "' width='730px' height='364px' align='middle'/>"
+    );
+}
+function makeButtons() {
+    var allCameras = core.Cameras();
+    return allCameras.map(function(camera) {
+        return (
+            "<div class='row'><button class='btn btn-danger' id='" +
+            camera +
+            "'>" +
+            camera +
+            '</button></div>'
+        );
+    });
+}
+function gameStart(Model) {}
+function ChooseMonsterLocation(location) {
+    var index = Math.floor(Math.random() * location.length);
+    return location[index];
+}
+
+function main() {
+    var audio = new Audio('../../assets/Sound_Effects/Button_Click.ogx');
+    var game = core.init();
+    $('#buttons').html(makeButtons());
+    $('#bathroom').click(function() {
+        audio.play();
+        core.lookingAt(game, 'bathroom');
+        view(game);
+    });
+    $('#office').click(function() {
+        audio.play();
+        core.lookingAt(game, 'office');
+        view(game);
+    });
+    $('#classroom').click(function() {
+        audio.play();
+        core.lookingAt(game, 'classroom');
+        view(game);
+    });
+    $('#hallway').click(function() {
+        audio.play();
+        core.lookingAt(game, 'hallway');
+        view(game);
+    });
+    $('#screen').html(screenView(core.SceneChange()['Room']['classroom']));
+    setInterval(function() {
+        game.Power -= 1;
+        $('.jumbotron').html('<h1> Power: ' + game.Power + '</h1>');
+    }, 3000);
+    setInterval(function() {
+        monsterMove(game);
+        $('#screen').html(screenView('../../assets/Tv_Effects/Static_Tv.gif'));
+        setTimeout(function() {
+            view(game);
+        }, 1000);
+        console.log(game.monsterLocation);
+    }, 5000);
+    view(game);
+}
+$(main);
